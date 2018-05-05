5810b3e6456b8b29b5cfdc0340209ae411dd74bc
Kendrick Lee <ken132132@gmail.com>
2018-05-05 19:55:22 -0400

    Turned into function

--- Scrape.py
+++ Scrape.py
@@ -4,32 +4,35 @@ from bs4 import BeautifulSoup
 
 #example results page to test scraping 
 results_page = 'https://www.kijiji.ca/b-oakville-halton-region/3ds/k0l1700277?dc=true'
-page = urlopen(results_page)
 
 #parse using beautiful soup
-parsed_page = BeautifulSoup(page, 'html.parser')
+def scrape(results, upper_price):
+    page = urlopen(results)
+    parsed_page = BeautifulSoup(page, 'html.parser')
 
-kijiji_ads = parsed_page.find_all('div',{'class': 'regular-ad'})
-# Take out the <div> of name and get its value
+    kijiji_ads = parsed_page.find_all('div',{'class': 'regular-ad'})
 
-prices = []
-ad_id = []
-url_list = []
-x = 0
+    prices = []
+    ad_id = []
+    url_list = []
 
-for ad in kijiji_ads:
-    price_box = ad.find('div',{'class': 'price'})
-    ad_url = ad.find('a',{'class': 'title'})
-    link = 'https://www.kijiji.ca' + ad_url['href']
+    for ad in kijiji_ads:
+    
+        price_box = ad.find('div',{'class': 'price'})
+        ad_url = ad.find('a',{'class': 'title'})
+        link = 'https://www.kijiji.ca' + ad_url['href']
+        price = price_box.text.strip()
 
-    price = price_box.text.strip()
-    if "Please Contact" in price:
-      continue
-    removed_currency = price[1:]
+        if "Please Contact" in price:
+            continue
 
-    if float(removed_currency) < 100:
-         prices.append(removed_currency)
-         url_list.append(link)
+        removed_currency = price[1:]
+        if float(removed_currency) < upper_price:
+            prices.append(removed_currency)
+            url_list.append(link)
 
-print(prices)
-print(url_list)
+    print(prices)
+    print(url_list)
+
+scrape(results_page, 100)
+
